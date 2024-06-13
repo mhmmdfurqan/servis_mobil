bbc7fbfb1410ec0c00a69908e5e18b431d76f19a
mhmmdfurqan <mhd.furqan76@gmail.com>
2024-06-14 01:15:59 +0700

    memunculkan modal ketika di klik tombol hapus

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -29,7 +29,30 @@
                                         <td>{{$item->telp_customer}}</td>  
                                             <td>
                                                 <a href="/customer/edit/{{$item->id}}" class="btn btn-info btn-sm"><em class="bi bi-pencil"></em></a>
-                                                <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#"><em class="bi bi-trash"></em></button>                                               
+                                                <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#"><em class="bi bi-trash"></em></button>
+                                                <div class="modal fade" tabindex="-1" id="#">    
+                                                    <div class="modal-dialog" role="document">        
+                                                        <div class="modal-content">            
+                                                            <a href="customer" class="close" data-dismiss="modal" aria-label="Close">                
+                                                                <em class="icon ni ni-cross"></em>            
+                                                            </a>            
+                                                            <div class="modal-header">                
+                                                                <h5 class="modal-title">Peringatan!</h5>            
+                                                            </div>            
+                                                            <div class="modal-body">                
+                                                                Yakin Data Customer {{$item->nama_customer}} di Hapus?
+                                                            </div>            
+                                                            <div class="modal-footer bg-light">                
+                                                            <a href="customer" class="btn btn-secondary" data-dismiss="modal">Batal</a>
+                                                            <form action="/customer/{{$item->id}}" method="post">
+                                                                @method('DELETE')
+                                                                @csrf
+                                                                <button type="submit" class="btn btn-danger">Hapus</button>
+                                                            </form>                                                                   
+                                                            </div>        
+                                                        </div>    
+                                                    </div>
+                                                </div>                                               
                                             </td>
                                         </tr>                                        
                                         @empty
