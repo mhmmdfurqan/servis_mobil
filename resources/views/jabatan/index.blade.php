f7d491fe8f1fc194ab58439b944418759d211f10
mhmmdfurqan <mhd.furqan76@gmail.com>
2024-06-14 03:57:06 +0700

    memunculkan modal ketika di klik tombol hapus jabatan

--- resources/views/jabatan/index.blade.php
+++ resources/views/jabatan/index.blade.php
@@ -26,6 +26,29 @@
                                             <td>
                                             <a href="/jabatan/edit/{{$item->id}}" class="btn btn-info btn-sm"><em class="bi bi-pencil"></em></a>
                                             <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#"><em class="bi bi-trash"></em></button>
+                                            <div class="modal fade" tabindex="-1" id="#">    
+                                                    <div class="modal-dialog" role="document">        
+                                                        <div class="modal-content">            
+                                                            <a href="jabatan" class="close" data-dismiss="modal" aria-label="Close">                
+                                                                <em class="icon ni ni-cross"></em>            
+                                                            </a>            
+                                                            <div class="modal-header">                
+                                                                <h5 class="modal-title">Peringatan!</h5>            
+                                                            </div>            
+                                                            <div class="modal-body">                
+                                                                Yakin Data Jabatan {{$item->jabatan}} di Hapus?
+                                                            </div>            
+                                                            <div class="modal-footer bg-light">                
+                                                            <a href="jabatan" class="btn btn-secondary" data-dismiss="modal">Batal</a>
+                                                            <form action="/jabatan/{{$item->id}}" method="post">
+                                                                @method('DELETE')
+                                                                @csrf
+                                                                <button type="submit" class="btn btn-danger">Hapus</button>
+                                                            </form>                                                                   
+                                                            </div>        
+                                                        </div>    
+                                                    </div>
+                                                </div>
                                             </td>
                                         </tr>
                                         @empty
