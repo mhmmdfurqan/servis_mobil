b3767612940ac7d4353616251e73f2820be060ca
mhmmdfurqan <mhd.furqan76@gmail.com>
2024-06-14 00:12:31 +0700

    membuat migration
    - customer
    - jabatan
    - petugas
    - servis
    - pembayaran

--- database/migrations/2024_06_13_170614_create_serviss_table.php
+++ database/migrations/2024_06_13_170614_create_serviss_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('serviss', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses', ['admin', 'user'])->default('user');
-            $table->rememberToken();
+            $table->bigInteger('customer_id');
+            $table->string('no_polisi',10)->unique();
+            $table->bigInteger('petugas_id');
+            $table->date('tanggal_servis');
+            $table->bigInteger('perbaikan_id');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('serviss');
     }
 };
--- database/migrations/2024_06_13_170626_create_pembayarans_table.php
+++ database/migrations/2024_06_13_170626_create_pembayarans_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pembayarans', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses', ['admin', 'user'])->default('user');
-            $table->rememberToken();
+            $table->bigInteger('servis_id');
+            $table->string('tanggal_pembayaran')->unique();
+            $table->bigInteger('petugas_id');    
+            $table->string('total_harga',100);
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pembayarans');
     }
 };
