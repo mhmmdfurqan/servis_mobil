9e55492508f7de59cf3380c99a73c801d60f80ad
mhmmdfurqan <mhd.furqan76@gmail.com>
2024-06-15 02:47:10 +0700

    memunculkan modal ketika di klik tombol hapus petugas

--- resources/views/petugas/index.blade.php
+++ resources/views/petugas/index.blade.php
@@ -31,6 +31,29 @@
                                             <td>
                                             <a href="/petugas/edit/{{$item->id}}" class="btn btn-info btn-sm"><em class="bi bi-pencil"></em></a>
                                             <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#"><em class="bi bi-trash"></em></button>
+                                            <div class="modal fade" tabindex="-1" id="#">    
+                                                    <div class="modal-dialog" role="document">        
+                                                        <div class="modal-content">            
+                                                            <a href="petugas" class="close" data-dismiss="modal" aria-label="Close">                
+                                                                <em class="icon ni ni-cross"></em>            
+                                                            </a>            
+                                                            <div class="modal-header">                
+                                                                <h5 class="modal-title">Peringatan!</h5>            
+                                                            </div>            
+                                                            <div class="modal-body">                
+                                                                Yakin Data Petugas {{$item->nama_petugas}} di Hapus?
+                                                            </div>            
+                                                            <div class="modal-footer bg-light">                
+                                                            <a href="petugas" class="btn btn-secondary" data-dismiss="modal">Batal</a>
+                                                            <form action="/petugas/{{$item->id}}" method="post">
+                                                                @method('DELETE')
+                                                                @csrf
+                                                                <button type="submit" class="btn btn-danger">Hapus</button>
+                                                            </form>                                                                   
+                                                            </div>        
+                                                        </div>    
+                                                    </div>
+                                                </div>
                                             </td>
                                         </tr>
                                         @empty
