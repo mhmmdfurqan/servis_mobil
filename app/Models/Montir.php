7db45a84386005639fc58d44404767e1966eb3af
mhmmdfurqan <mhd.furqan76@gmail.com>
2024-08-05 17:45:45 +0700

    montir part 1
    membuat controller montir
    membuat model montir

---
+++ app/Models/Montir.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Montir extends Model
+{
+    use HasFactory;
+
+    public function servis(): BelongsTo
+    {
+        return $this->belongsTo(Servis::class);
+    }
+}
